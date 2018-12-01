346511b26bce60ddcee9255a286c77f8276a3eba
Per <perjonaserik@gmail.com>
2018-12-01 13:41:36 +0100

    UI score

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    [SerializeField] private Text scoreText;
+    public string ScoreText
+    {
+        get
+        {
+            return scoreText.text;
+        }
+        set
+        {
+            if (scoreText != null)
+            scoreText.text = value;
+        }
+    }
+
+    private void Update()
+    {
+        ScoreText = Time.timeSinceLevelLoad.ToString();
+    }
+}
