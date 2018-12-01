f455d13e03220ffc37c502ae4b14ae191d533e74
Per <perjonaserik@gmail.com>
2018-12-01 16:04:21 +0100

    ArbetsplatsPrefab

---
+++ Assets/ArbetsplatsScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArbetsplatsScript : MonoBehaviour {
+
+    [SerializeField] private bool hasWorker = false;
+    [SerializeField] private bool hasAwesomeWorker = false;
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (IsPlayer(col))
+            AddWorker();
+    }
+
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (IsPlayer(col))
+            RemoveWorker();
+    }
+
+    private bool IsPlayer(Collider2D col)
+    {
+        return (col.tag == "Player");
+    }
+
+    public void AddWorker()
+    {
+        hasWorker = true;
+        hasAwesomeWorker = (Random.value > 0.5f); //Hälften av gångerna är det en awesome person
+    }
+
+    public void RemoveWorker()
+    {
+        hasWorker = false;
+    }
+}
