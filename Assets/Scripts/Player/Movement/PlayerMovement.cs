7014a4f1abe12252d5e0ff3adc05c27adb8679af
Maakep <schon.fredrik@gmail.com>
2018-12-01 17:40:01 +0100

    Update stair prefab

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -10,52 +10,107 @@ public class PlayerMovement : MonoBehaviour {
 	private bool StairsActivated = false;
 	private bool OnWalkDownArea = false;
 	private bool OnWalkUpArea = false;
+	private bool TouchingRightWall = false;
+	private bool TouchingLeftWall = false;
 
-	void Start () {
-		
+	private bool CanWalkVertical { 
+		get { 
+			return OnStairs || OnWalkDownArea || OnWalkUpArea;
+	 	} 
 	}
 	
-	void Update () {
-		float y = 0;
-		if (OnStairs || OnWalkDownArea || OnWalkUpArea) {
-			y = Input.GetAxisRaw("Vertical") * speed;
-			
-			if (OnWalkDownArea && !OnStairs) {
-				if (y > 0) {
-					y = 0;
-				}
-			}
-
-			if (OnWalkUpArea && !OnStairs) {
-				if (y < 0) {
-					y = 0;
-				}
-			}
-
-			if (y != 0) {
-				StairsActivated = true;
-			}
+	private bool CanWalkHorizontal {
+		get {
+			return !StairsActivated;
 		}
-
-		float x = 0;
-		if (!StairsActivated) {
-			 x = Input.GetAxisRaw("Horizontal") * speed;
-		}
-
-		transform.Translate(x, y, 0);
 	}
+
+	void Update () {
+		Vector2 velocity = new Vector2(0, 0);
+
+		if (CanWalkHorizontal) {
+			velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+			velocity = HandleLeftWall(velocity);
+			velocity = HandleRightWall(velocity);
+		}
+
+		if (CanWalkVertical) {
+			velocity.y = Input.GetAxisRaw("Vertical") * speed;
+			velocity = HandleWalkDown(velocity);
+			velocity = HandleWalkUp(velocity);
+			velocity = HandleStairs(velocity);
+		}
+
+		transform.Translate(velocity);
+	}
+
+#region collision movement restrictions
+	Vector2 HandleWalkDown(Vector2 vel) {
+		if (OnWalkDownArea && !OnStairs)
+			if (vel.y >= 0) {
+				vel.y = 0;
+			}
+
+		return vel;
+	}
+
+	Vector2 HandleWalkUp(Vector2 vel) {
+		if (OnWalkUpArea && !OnStairs)
+			if (vel.y <= 0) {
+				vel.y = 0;
+			}
+
+		return vel;
+	}
+
+	Vector2 HandleStairs(Vector2 vel) {
+		float angle = 0.74f;
+		if (vel.y != 0) {
+			StairsActivated = true;
+		}
+		if (vel.y > 0) {
+			vel.x = speed * angle;
+		}
+		if (vel.y < 0) {
+			vel.x = -speed * angle;
+		}
+		return vel;
+	}
+
+	Vector2 HandleRightWall(Vector2 vel) {
+		if (TouchingRightWall)
+			if (vel.x > 0) {
+				vel.x = 0;
+			}
+
+		return vel;
+	}
+
+	Vector2 HandleLeftWall(Vector2 vel) {
+		if (TouchingLeftWall)
+			if (vel.x < 0)
+				vel.x = 0;
+
+		return vel;
+	}
+#endregion
 
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag == "Stairs") 				OnStairs = true;
 		if (col.tag == "WalkDownArea") 	OnWalkDownArea = true;
 		if (col.tag == "WalkUpArea") 		OnWalkUpArea = true;
+		if (col.tag == "LeftWall")			TouchingLeftWall = true;
+		if (col.tag == "RightWall")			TouchingRightWall = true;
 	}
 
 	void OnTriggerExit2D(Collider2D col) {
+		Debug.Log(col.tag);
 		if (col.tag == "Stairs") 				OnStairs = false;
 		if (col.tag == "Stairs") 				StairsActivated = false;
 		if (col.tag == "WalkDownArea") 	OnWalkDownArea = false;
 		if (col.tag == "WalkUpArea") 		OnWalkUpArea = false;
+		if (col.tag == "LeftWall")			TouchingLeftWall = false;
+		if (col.tag == "RightWall")			TouchingRightWall = false;
 	}
 }
