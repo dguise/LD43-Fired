af521087d33124949e9f23c583347a1c004ce8c9
Per <perjonaserik@gmail.com>
2018-12-02 14:05:49 +0100

    Player - Visuell feedback på avsked

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     [Range(0.01f, 0.1f)]
     public float speed = 0.05f;
 
@@ -16,20 +17,26 @@ public class PlayerMovement : MonoBehaviour {
     private bool TouchingLeftWall = false;
 
     private SpriteRenderer sr;
-    [SerializeField]private SpriteRenderer srChild;
-	private Animator anim;
+    [SerializeField] private SpriteRenderer srChild;
+    [SerializeField] private GameObject babyPrefab;
+    private float kickingPowah = 7.0f;
+    private Animator anim;
 
-	private bool CanWalkVertical { 
-		get { 
-			return OnStairs || OnWalkDownArea || OnWalkUpArea;
-	 	} 
-	}
-	
-	private bool CanWalkHorizontal {
-		get {
-			return !StairsActivated;
-		}
-	}
+    private bool CanWalkVertical
+    {
+        get
+        {
+            return OnStairs || OnWalkDownArea || OnWalkUpArea;
+        }
+    }
+
+    private bool CanWalkHorizontal
+    {
+        get
+        {
+            return !StairsActivated;
+        }
+    }
 
     public bool CarryingWorker
     {
@@ -37,37 +44,41 @@ public class PlayerMovement : MonoBehaviour {
     }
 
 
-    void Start() {
-		sr = GetComponent<SpriteRenderer>();
-		anim = GetComponent<Animator>();
-	}
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
 
 
-	void Update () {
-		Vector2 velocity = new Vector2(0, 0);
+    void Update()
+    {
+        Vector2 velocity = new Vector2(0, 0);
 
-		if (CanWalkHorizontal) {
-			velocity.x = Input.GetAxisRaw("Horizontal") * speed;
-			velocity = HandleLeftWall(velocity);
-			velocity = HandleRightWall(velocity);
-		}
-
-		if (CanWalkVertical) {
-			velocity.y = Input.GetAxisRaw("Vertical") * speed * stairSpeedModifier;
-			velocity = HandleWalkDown(velocity);
-			velocity = HandleWalkUp(velocity);
-			velocity = HandleStairs(velocity);
-		}
-
-		var isRunning = velocity.x != 0;
-		anim.SetBool("Running", isRunning);
-		if (isRunning)
+        if (CanWalkHorizontal)
         {
-			sr.flipX = velocity.x > 0;
+            velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+            velocity = HandleLeftWall(velocity);
+            velocity = HandleRightWall(velocity);
+        }
+
+        if (CanWalkVertical)
+        {
+            velocity.y = Input.GetAxisRaw("Vertical") * speed * stairSpeedModifier;
+            velocity = HandleWalkDown(velocity);
+            velocity = HandleWalkUp(velocity);
+            velocity = HandleStairs(velocity);
+        }
+
+        var isRunning = velocity.x != 0;
+        anim.SetBool("Running", isRunning);
+        if (isRunning)
+        {
+            sr.flipX = velocity.x > 0;
             srChild.flipX = !sr.flipX;
             srChild.transform.localPosition = (srChild.flipX)
-				? new Vector3(0.34f, srChild.transform.localPosition.y, srChild.transform.localPosition.z) 
-				: new Vector3(-0.34f, srChild.transform.localPosition.y, srChild.transform.localPosition.z);
+                ? new Vector3(0.34f, srChild.transform.localPosition.y, srChild.transform.localPosition.z)
+                : new Vector3(-0.34f, srChild.transform.localPosition.y, srChild.transform.localPosition.z);
         }
 
         transform.Translate(velocity);
@@ -77,7 +88,13 @@ public class PlayerMovement : MonoBehaviour {
             foreach (var apl in GameObject.FindGameObjectsWithTag(Tags.Arbetsplats))
             {
                 if (apl.GetComponentInChildren<Renderer>().bounds.Intersects(this.GetComponent<Renderer>().bounds))
-                    apl.GetComponent<ArbetsplatsScript>().RemoveWorker();
+                {
+                    if (apl.GetComponent<ArbetsplatsScript>().Aktiv)
+                    {
+                        apl.GetComponent<ArbetsplatsScript>().RemoveWorker();
+                        KickTheBaby();
+                    }
+                }
             }
 
             foreach (var shop in GameObject.FindGameObjectsWithTag(Tags.PlaceToGetWorkers))
@@ -87,7 +104,14 @@ public class PlayerMovement : MonoBehaviour {
                         srChild.gameObject.SetActive(true);
             }
         }
-            
+    }
+
+    private void KickTheBaby()
+    {
+        var baby = (GameObject)GameObject.Instantiate(babyPrefab, this.transform.position, transform.rotation);
+        baby.GetComponent<Rigidbody2D>().simulated = true;
+        baby.GetComponent<Rigidbody2D>().AddForce(new Vector2(this.transform.position.x < 0 ? -kickingPowah : kickingPowah, 3), ForceMode2D.Impulse);
+        GameObject.Destroy(baby, 3.0f);
     }
 
     public void DropOffWorker()
@@ -96,71 +120,84 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     #region collision movement restrictions
-    Vector2 HandleWalkDown(Vector2 vel) {
-		if (OnWalkDownArea && !OnStairs)
-			if (vel.y >= 0) {
-				vel.y = 0;
-			}
+    Vector2 HandleWalkDown(Vector2 vel)
+    {
+        if (OnWalkDownArea && !OnStairs)
+            if (vel.y >= 0)
+            {
+                vel.y = 0;
+            }
 
-		return vel;
-	}
+        return vel;
+    }
 
-	Vector2 HandleWalkUp(Vector2 vel) {
-		if (OnWalkUpArea && !OnStairs)
-			if (vel.y <= 0) {
-				vel.y = 0;
-			}
+    Vector2 HandleWalkUp(Vector2 vel)
+    {
+        if (OnWalkUpArea && !OnStairs)
+            if (vel.y <= 0)
+            {
+                vel.y = 0;
+            }
 
-		return vel;
-	}
+        return vel;
+    }
 
-	Vector2 HandleStairs(Vector2 vel) {
-		float angle = 0.74f;
-		if (vel.y != 0) {
-			StairsActivated = true;
-		}
-		if (vel.y > 0) {
-			vel.x = speed * stairSpeedModifier * angle;
-		}
-		if (vel.y < 0) {
-			vel.x = -speed * stairSpeedModifier * angle;
-		}
-		return vel;
-	}
+    Vector2 HandleStairs(Vector2 vel)
+    {
+        float angle = 0.74f;
+        if (vel.y != 0)
+        {
+            StairsActivated = true;
+        }
+        if (vel.y > 0)
+        {
+            vel.x = speed * stairSpeedModifier * angle;
+        }
+        if (vel.y < 0)
+        {
+            vel.x = -speed * stairSpeedModifier * angle;
+        }
+        return vel;
+    }
 
-	Vector2 HandleRightWall(Vector2 vel) {
-		if (TouchingRightWall)
-			if (vel.x > 0) {
-				vel.x = 0;
-			}
+    Vector2 HandleRightWall(Vector2 vel)
+    {
+        if (TouchingRightWall)
+            if (vel.x > 0)
+            {
+                vel.x = 0;
+            }
 
-		return vel;
-	}
+        return vel;
+    }
 
-	Vector2 HandleLeftWall(Vector2 vel) {
-		if (TouchingLeftWall)
-			if (vel.x < 0)
-				vel.x = 0;
+    Vector2 HandleLeftWall(Vector2 vel)
+    {
+        if (TouchingLeftWall)
+            if (vel.x < 0)
+                vel.x = 0;
 
-		return vel;
-	}
-#endregion
+        return vel;
+    }
+    #endregion
 
 
-	void OnTriggerEnter2D(Collider2D col) {
-		if (col.tag == Tags.Stairs) 		OnStairs = true;
-		if (col.tag == Tags.WalkDownArea)  	OnWalkDownArea = true;
-		if (col.tag == Tags.WalkUpArea) 	OnWalkUpArea = true;
-		if (col.tag == Tags.LeftWall)       TouchingLeftWall = true;
-		if (col.tag == Tags.RightWall)		TouchingRightWall = true;
-	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == Tags.Stairs) OnStairs = true;
+        if (col.tag == Tags.WalkDownArea) OnWalkDownArea = true;
+        if (col.tag == Tags.WalkUpArea) OnWalkUpArea = true;
+        if (col.tag == Tags.LeftWall) TouchingLeftWall = true;
+        if (col.tag == Tags.RightWall) TouchingRightWall = true;
+    }
 
-	void OnTriggerExit2D(Collider2D col) {
-		if (col.tag == Tags.Stairs) 		OnStairs = false;
-		if (col.tag == Tags.Stairs) 		StairsActivated = false;
-		if (col.tag == Tags.WalkDownArea) 	OnWalkDownArea = false;
-		if (col.tag == Tags.WalkUpArea) 	OnWalkUpArea = false;
-		if (col.tag == Tags.LeftWall)		TouchingLeftWall = false;
-		if (col.tag == Tags.RightWall)		TouchingRightWall = false;
-	}
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == Tags.Stairs) OnStairs = false;
+        if (col.tag == Tags.Stairs) StairsActivated = false;
+        if (col.tag == Tags.WalkDownArea) OnWalkDownArea = false;
+        if (col.tag == Tags.WalkUpArea) OnWalkUpArea = false;
+        if (col.tag == Tags.LeftWall) TouchingLeftWall = false;
+        if (col.tag == Tags.RightWall) TouchingRightWall = false;
+    }
 }
---
+++ Assets/Scripts/RotateOverTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour {
+
+    public float roationSpeed = 3.0f;
+	void Update ()
+    {
+        this.transform.Rotate(Vector3.forward * (roationSpeed * Time.deltaTime));
+    }
+}
