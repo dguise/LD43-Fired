7bced6ce9079de8ec6dd2101fceb8346f2c30d36
Per <perjonaserik@gmail.com>
2018-12-02 12:12:04 +0100

    Kamerascript

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    float startingY = 0;
+
+    private void Start()
+    {
+        if (Player == null)
+            Player = GameObject.FindGameObjectWithTag(Tags.Player);
+        startingY = this.transform.position.y - Player.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, startingY + Player.transform.position.y, this.transform.position.z);
+    }
+
+}
