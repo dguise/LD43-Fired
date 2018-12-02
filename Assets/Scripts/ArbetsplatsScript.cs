84ec6cc711699a8fc7fdce43aa1b88a48199a556
Per <perjonaserik@gmail.com>
2018-12-02 11:45:54 +0100

    Arbetsplatser - AwesomeArbetare svarar rätt, icke awesome drar en Erik

--- Assets/Scripts/ArbetsplatsScript.cs
+++ Assets/Scripts/ArbetsplatsScript.cs
@@ -32,8 +32,14 @@ public class ArbetsplatsScript : MonoBehaviour
         if (IsPlayer(col))
         {
             if (Aktiv)
-                GUIManager.ShowDialogue(listOfQuestions[Random.Range(0, listOfQuestions.Count - 1)],
-                                        listOfAnswers[Random.Range(0, listOfAnswers.Count - 1)]);
+            {
+                int indexOfQA = Random.Range(0, 
+                    (listOfQuestions.Count <= listOfAnswers.Count ? listOfQuestions.Count - 1 : listOfAnswers.Count - 1));  //Om de är olika längd så utgår vi ifrån kortaste av de 2 så att vi har en rad i båda listorna
+                int indexOfStoopidA = Random.Range(0, listOfErik.Count);
+                GUIManager.ShowDialogue(listOfQuestions[indexOfQA],
+                                        HasAwesomeWorker ? listOfAnswers[indexOfQA] : indexOfStoopidA < listOfErik.Count - 1 ? listOfErik[indexOfStoopidA] : Random.Range(1, 101).ToString());
+
+            }
             else if (col.gameObject.GetComponent<PlayerMovement>().CarryingWorker)
             {
                 AddWorker();
@@ -103,60 +109,73 @@ public class ArbetsplatsScript : MonoBehaviour
         CancelInvoke("DoStuffBadWorkersDo");
     }
 
-    public List<string> listOfQuestions = new List<string> { "What was the last thing you created?",
-"Who is a better cook your mother or grandmother?",
-"What book do you wish would be turned into a movie?",
-"Is there an app that you hate but use anyways?",
-"What was the last thing that you fixed?",
-"Could you survive in the wilderness for a month ? ",
-"What are some songs you know by heart ? ",
-"What piece of technology brings you the most joy ? ",
-"If you could rename the street you lived on, what would you rename it ? ",
-"What do your clothes say about you ? ",
-"What’s your “going to bed” routine ? ",
-"What animal best represents your personality ? ",
-"What’s the fullest you’ve ever been ? ",
-"What behaviors make you think a person is creepy ? ",
-"Who in the movie business seems the most down to earth ? ",
-"What’s the funniest or most amazing cell phone cover you have seen ? ",
-"Which musical artist is greatly overrated ? ",
-"What company or franchise do you wish would go out of business ? ",
-"What’s the perfect temperature to set the thermostat at ? ",
-"Who around you has the worst luck ? ",
-"What’s your favorite sounding word ? ",
-"What’s the weirdest thing that has happened to you in a car ? ",
-"If you could fit your whole life into one picture what would it look like ? ",
-"What’s something that can’t be found or bought on the internet ? ",
-"What should the first colony on another planet be called ? ",
-"If you put out a magazine, what would you name it and what would be in it ? ",
-"If you were challenged to a duel, what weapons would you choose ? ",
-"What villain do you really feel for?",
-"What is the most interesting thing you could do with 400 pounds of cheddar cheese ? ",
-"What did you think you were good at but are actually quite bad at ? ",
-"What in a trailer automatically makes you assume a movie will be horrible ? ",
-"What game do you wish you could act out in real life ? ",
-"What are you really happy about being terrible at ? ",
-"What would be the funniest thing to fill a piñata with ?}", };
+    public List<string> listOfQuestions = new List<string> {
+"How are you today?",
+"Any problems here?",
+"How are the kids?",
+"Did you resolve that issue we had?",
+"How do you like it here?",
+"Could you help me with something?",
+"What do you think about our company?",
+"Any suggestions on how to improve our sales?",
+"What did you have for lunch?",
+"Are you satisfied with your collegues?",
+"Do you feel that your tasks are challenging enough?",
+"Do you know what time it is?",
+"We have a meeting soon.",
+"You are here early today.",
+"Have you finished your report?",
+"The company is doing bad right now and we might have to get rid of people.",
+"I am collecting money for a gift to Steve's birthday.",};
 
 
-    public List<string> listOfAnswers = new List<string> { "We need to rent a room for our party.",
-"My Mum tries to be cool by saying that she likes all the same things that I do.",
-"He didn’t want to go to the dentist, yet he went anyway.",
-"The body may perhaps compensates for the loss of a true metaphysics.",
-"He told us a very exciting adventure story.",
-"Malls are great places to shop; I can find everything I need under one roof.",
-"She did not cheat on the test, for it was not the right thing to do.",
-"If I don’t like something, I’ll stay away from it.",
-"Everyone was busy, so I went to the movie alone.",
-"The book is in front of the table.",
-"Let me help you with your baggage.",
-"Is it free?",
-"She did her best to help him.",
-"She advised him to come back at once.",
-"I am happy to take your donation; any amount will be greatly appreciated.",
-"The lake is a long way from here.",
-"There was no ice cream in the freezer, nor did they have money to go to the store.",
-"Sixty-Four comes asking for bread.",
-"They got there early, and they got really good seats.",
-"We have never been to Asia, nor have we visited Africa.",};
+    public List<string> listOfAnswers = new List<string> {
+"-Fine how are you?",
+"-Not at all!",
+"-Great!",
+"-Yes, it was tricky but we found a good solution.",
+"-This workplace is great!",
+"-Yes of course, let me just finish up here.",
+"-Very nice and feels good to do something important.",
+"-Acctually, I have a small list of suggestions here if you want to have a look?",
+"-Todays lunch at the cantina. I can recommend that.",
+"-Yes, they are very helpful and have plenty of experience.",
+"-I think there is a good balance.",
+"-It is time to work, haha!",
+"-Yes, I will be there in time.",
+"-I have a lot to do these days.",
+"-Almost done!",
+"-That sounds bad, maybe some extra work will help.",
+"-Here is something from me too.",};
+
+    public List<string> listOfErik = new List<string> {
+"Bananas!",
+"I hate you.",
+"...no.",
+"&%?$@!",
+"AAAAAaargh",
+"Goff goff",
+"error syntax",
+"My collegues smell bad.",
+"I can not help you right now.",
+"Stay a while and listen.",
+"The important thing is that we are happy.",
+"What?",
+"...",
+"No?",
+"Yes.",
+"Sounds great.",
+"Ok.",
+"Where am I?",
+"Who am I?",
+"Too... many.... numbers...",
+"LOL",
+"Please, do not talk to me.",
+"Do not watch my screen",
+"I do not have to answer that!",
+"Zzzzzz",
+"Crush you enemies, see them driven before you, and hear the lamentations of their women.",
+"Why?",
+"help",};
+
 }
