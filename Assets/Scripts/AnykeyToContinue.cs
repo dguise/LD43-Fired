399774834ca53c0c928e724819580f1b5f68d99d
Per <perjonaserik@gmail.com>
2018-12-02 14:46:58 +0100

    TitleScene, InfoScene, GameOverScene

---
+++ Assets/Scripts/AnykeyToContinue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnykeyToContinue : MonoBehaviour
+{
+    bool doOnce = true;
+    void Update()
+    {
+        if (doOnce)
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                doOnce = false;
+            }
+        }
+    }
+}
